143d41d0e9c694f2f7e32ba354d360d3a79fe4dd
Brady <brady.savarie@hotmail.com>
2023-01-02 13:47:18 -0500

    Update display font again, write arithmetic functions

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ function displayValue(value) {
 }
 displayValue(currentValue);
 
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function percent(a) {
+    return a / 100;
+};
+
